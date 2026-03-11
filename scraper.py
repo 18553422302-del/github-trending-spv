c3fe50a137e20a20f8e7780551e6b8ce26a7b91a
18553422302-del <18553422302@163.com>
2026-03-11 13:39:05 +0800

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+from datetime import datetime
+
+def scrape_trending():
+# 伪装成真实的浏览器“受体”（客户端标识），避免被直接拦截
+headers = {
+"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+"Accept-Language": "zh-CN,zh;q=0.9"
+}
+url = ""
+
+if name == "main":
+scrape_trending()
